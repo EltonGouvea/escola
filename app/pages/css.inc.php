d52477c84ce010d78a6637566279f3202f9ced00
EltonGouvea <tomgouvea@gmail.com>
2018-09-06 20:01:06 +0000

    novo

---
+++ app/pages/css.inc.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="../../vendor/bootstrap/css/bootstrap.min.css">
+<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href="app/css/escola.inc.css" rel="stylesheet">
